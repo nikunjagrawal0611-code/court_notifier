8370cc7effabcc8e763287f5cbb3a8faf31e50a2
nikunjagrawal0611-code <nikunjagrawal8680@gmail.com>
2026-03-15 14:05:43 +0530

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -16,7 +16,7 @@ CHAT_ID = os.environ["CHAT_ID"]
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     r = requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
-    print(r.text))
+    print(r.text)
 
 
 # 🔔 Send start notification
