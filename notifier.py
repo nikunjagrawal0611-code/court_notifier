c3311d25a5da14b82327e67e3929635b2366e068
nikunjagrawal0611-code <nikunjagrawal8680@gmail.com>
2026-03-15 14:03:07 +0530

    Print response from Telegram API on message send
    Added response printing for Telegram message sending.

--- notifier.py
+++ notifier.py
@@ -15,7 +15,8 @@ CHAT_ID = os.environ["CHAT_ID"]
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+    r = requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+    print(r.text))
 
 
 # 🔔 Send start notification
